abb26355d073f0930ce582fe5bf5523b73c7215b
sehomi <sehomi755@gmail.com>
2022-11-11 18:50:30 +0330

    action space and state space are prepared

--- RL1.py
+++ RL1.py
@@ -5,15 +5,13 @@ Created on Fri Nov  4 14:53:26 2022
 @author: Paytakht
 """
 
-# !pip install stable-baselines[mpi]==2.10.2
-# !pip install stable-baselines3[extra]
-
 from gym.spaces import Box, MultiDiscrete, Discrete
 from gym import Env
 from gym import spaces
 from sumolib import checkBinary
 import gym
 import traci
+import traci.constants as tc
 import os
 import sys
 import numpy as np
@@ -24,6 +22,7 @@ import xml.etree.ElementTree as ET
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
+    print("SUMO_HOME was found. {}".format(os.environ['SUMO_HOME']))
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
@@ -34,9 +33,8 @@ class CustomEnv(Env):
     def __init__(self):
         # super().__init__(self, CustomEnv)
         # super(CustomEnv, self).__init__()
-        self.sumoBinary = checkBinary('sumo')  # adding -gui  opens sumo-gui
-        self.sumoCmd = [self.sumoBinary, "-c", "/content/TrafficControl/sumo/LnCanada2.sumocfg"]
-        self.time = 0
+        self.sumoBinary = checkBinary('sumo-gui')  # adding -gui  opens sumo-gui
+        self.sumoCmd = [self.sumoBinary, "-c", "C:/Users/a/Desktop/TrafficControl/sumo/LnCanada2.sumocfg"]
         self.action_space = spaces.Discrete(2)
         # Set the action space to 2 discrete values i.e green, red
         # Action 1: green
@@ -44,10 +42,52 @@ class CustomEnv(Env):
         self.observation_space = spaces.Box(low=0, high=1000,
                                             shape=(), dtype=np.uint8)
 
+    def initializeDimensions(self):
+        
+        self.actionSpaceDims = len(self.trafficLights)
+        self.stateSpaceDims = len(self.trafficLights) + 1
+        
+        
+    def initializeVariables(self):
+        
+        self.trafficLights = traci.trafficlight.getIDList()
+        self.time = 0
+        self.initializeDimensions()
+        
+                
+    def getTrafficLightsQueuesLenghts(self):
+    
+        queues_lens = []
+        for tl in self.trafficLights:
+            
+            lanes = traci.trafficlight.getControlledLanes(tl)
+            
+            tl_halting_vehicles = 0
+            
+            for lane in lanes:
+                haltingVehicles = traci.lane.getLastStepHaltingNumber(lane)
+                tl_halting_vehicles += haltingVehicles
+            
+            queues_lens.append(tl_halting_vehicles)
+            
+        return queues_lens  
+    
+
+    def setTrafficLightStates(self, actions):
+        
+        assert len(actions) == self.actionSpaceDims
+        
+        tlstates = ['gggggggggggggggg' if a else 'rrrrrrrrrrrrrrrr' for a in actions]
+        
+        for i, tl in enumerate(self.trafficLights):
+            traci.trafficlight.setRedYellowGreenState(tl, tlstates[i])
+            
     #-------------------------------------------------------------------------       
     def reset(self):
         self.time = 0
-
+        traci.start(self.sumoCmd)
+        self.initializeVariables()
+        
         print("Environment Reset.")
     
     #-------------------------------------------------------------------------
@@ -57,16 +97,15 @@ class CustomEnv(Env):
     #-------------------------------------------------------------------------
     def step(self):
 
-        print("Step. time: {:d}".format(self.time))
+        # print("Step. time: {:d}".format(self.time))            
 
-        # part1
-        if self.time == 0 :
-            traci.start(self.sumoCmd)
-
-        #part2
         traci.simulationStep()
         self.time += 1
-        #part3
+    
+        self.setTrafficLightStates([1 for i in range(self.actionSpaceDims)])
+        ql = self.getTrafficLightsQueuesLenghts()
+        print(ql)
+        
         if self.time == 3600 :
             done = True
             traci.close()
@@ -83,7 +122,6 @@ print("Creating Environment.")
 
 env = CustomEnv()
 env.reset()
-
 done = False
 while not done :
     done = env.step()
